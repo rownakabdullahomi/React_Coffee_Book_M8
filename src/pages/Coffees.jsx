caa1886a95d27ecce841b2978c6455ad1248432d
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-11-03 01:46:22 +0600

    routes created for home coffees and dashboard

---
+++ src/pages/Coffees.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Coffees = () => {
+    return (
+        <div>
+            Coffees
+        </div>
+    );
+};
+
+export default Coffees;
--- src/routes/Routs.jsx
+++ src/routes/Routs.jsx
@@ -1,10 +1,27 @@
 import { createBrowserRouter } from "react-router-dom"
-import App from "../App"
+import MainLayout from "../layouts/MainLayout"
+import Home from "../pages/Home"
+import Coffees from "../pages/Coffees"
+import Dashboard from "../pages/Dashboard"
 
 const routes = createBrowserRouter([
     {
       path: "/",
-      element: <App></App>,
+      element: <MainLayout></MainLayout>,
+      children: [
+        {
+            path: "/",
+            element: <Home></Home>,
+        },
+        {
+            path: "/coffees",
+            element: <Coffees></Coffees>,
+        },
+        {
+            path: "dashboard",
+            element: <Dashboard></Dashboard>,
+        },
+      ]
     },
   ])
 
