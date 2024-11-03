0a70430499d8d6d56041cfc5f4bb4284fc2e0eb3
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-11-03 16:25:39 +0600

    dashboard data displayed

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,14 @@
+import { useEffect, useState } from "react";
 import Heading from "../components/Heading";
+import { getAllFavorites } from "../utility";
+import Card from "../components/Card";
 
 const Dashboard = () => {
+  const [coffees, setCoffees] = useState([]);
+  useEffect(() => {
+    const favorites = getAllFavorites();
+    setCoffees(favorites);
+  }, []);
   return (
     <div>
       <Heading
@@ -9,6 +17,12 @@ const Dashboard = () => {
           "Manage coffees that you have previously added as favorite. You can view or remove them from here."
         }
       ></Heading>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-12">
+        {coffees.map((coffee) => (
+          <Card key={coffee.id} coffee={coffee}></Card>
+        ))}
+      </div>
     </div>
   );
 };
