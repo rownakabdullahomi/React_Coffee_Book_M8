0d2ff679ab29f177b3ede2ae8318ce75167d387a
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-11-03 14:54:08 +0600

    dashboard title displayed dynamically

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import Heading from "../components/Heading";
 
 const Dashboard = () => {
-    return (
-        <div>
-            Dashboard
-        </div>
-    );
+  return (
+    <div>
+      <Heading
+        title={"Welcome to Dashboard"}
+        subtitle={
+          "Manage coffees that you have previously added as favorite. You can view or remove them from here."
+        }
+      ></Heading>
+    </div>
+  );
 };
 
 export default Dashboard;
